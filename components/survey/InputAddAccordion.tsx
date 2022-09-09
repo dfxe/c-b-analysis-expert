abfff004e7e3c172d9e4671c4a44b208aa2ae39f
Dragos X <29132359+uKruel@users.noreply.github.com>
2022-09-09 20:24:51 +0300

    Bump version

--- components/survey/InputAddAccordion.tsx
+++ components/survey/InputAddAccordion.tsx
@@ -1,17 +1,12 @@
 import React from "react";
+import { useDetails } from "../contexts/DetailsProvider";
 
 export default function InputAddAccordion() {
+  const details = useDetails();
   return (
-    <nav className="flex flex-col space-y-1">
-      <a
-        href=""
-        className="block px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-lg"
-      >
-        General
-      </a>
-
+    <nav className="flex flex-col space-y-1 ">
       <details className="group">
-        <summary className="flex items-center px-4 py-2 text-gray-500 rounded-lg cursor-pointer hover:bg-gray-100 hover:text-gray-700">
+        <summary className="flex border-2 items-center px-4 py-2 text-gray-500 rounded-lg cursor-pointer hover:bg-gray-100 hover:text-gray-700">
           <span className="text-sm font-medium"> Teams </span>
 
           <span className="ml-auto transition duration-300 shrink-0 group-open:-rotate-180">
@@ -30,81 +25,29 @@ export default function InputAddAccordion() {
           </span>
         </summary>
 
-        <nav className="flex flex-col mt-2 ml-8 space-y-1">
-          <a
-            href=""
-            className="block px-4 py-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-          >
-            Banned Users
-          </a>
-
-          <a
-            href=""
-            className="block px-4 py-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-          >
-            Calendar
-          </a>
+        <nav className="flex flex-col mt-2">
+          <div className="flex row justify-around">
+            <input
+              type="text"
+              className="block border-2 px-2 py-2 w-1/2 mr-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+              value="Server"
+            />
+            <input
+              type="text"
+              className="block border-2 px-4 py-2 w-1/2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
+              value={details.state.currency.chosenCurrency + " 20"}
+            />
+          </div>
         </nav>
-      </details>
 
-      <a
-        href=""
-        className="block px-4 py-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-      >
-        Billing
-      </a>
-
-      <a
-        href=""
-        className="block px-4 py-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-      >
-        Invoices
-      </a>
-
-      <details className="group">
-        <summary className="flex items-center px-4 py-2 text-gray-500 rounded-lg cursor-pointer hover:bg-gray-100 hover:text-gray-700">
-          <span className="text-sm font-medium"> Account </span>
-
-          <span className="ml-auto transition duration-300 shrink-0 group-open:-rotate-180">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-5 h-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </span>
-        </summary>
-
-        <nav className="flex flex-col mt-2 ml-8 space-y-1">
-          <a
-            href=""
-            className="block px-4 py-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-          >
-            Details
-          </a>
-
-          <a
-            href=""
-            className="block px-4 py-2 text-sm font-medium text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-          >
-            Security
-          </a>
-
-          <form action="/logout">
-            <button
-              type="submit"
-              className="w-full px-4 py-2 text-sm font-medium text-left text-gray-500 rounded-lg hover:bg-gray-100 hover:text-gray-700"
-            >
-              Logout
-            </button>
-          </form>
-        </nav>
+        <div className="flex row justify-around">
+          <button className="relative text-indigo-600 font-medium before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-left before:scale-x-0 before:bg-indigo-600 before:transition hover:before:scale-100">
+            Add field
+          </button>
+          <button className="relative text-indigo-600 font-medium before:absolute before:-bottom-1 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:bg-indigo-600 before:transition hover:before:scale-100">
+            Remove Field
+          </button>
+        </div>
       </details>
     </nav>
   );
